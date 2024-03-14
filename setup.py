ecac31f98362160b433d1b60fce0bfa91547c520
Matheus Eiji Faria Komatsu <matheuskomatsu@hotmail.com>
2024-03-14 18:53:16 -0300

    a
    a

--- setup.py
+++ setup.py
@@ -8,13 +8,13 @@ with open("requirements.txt") as f:
 
 setup(
     name="simple_math_package",
-    version="0.0.1",
+    version="0.0.2",
     author="matheus_eiji_faria_komatsu",
     author_email="matheuskomatsu@hotmail.com",
     description="This package is used for simple math operations. It's used as an exercise to understand how to create and distribute a package.",
     long_description=page_description,
     long_description_content_type="text/markdown",
-    url="https://github.com/MatheusKomatsu/simple-package-template",
+    url="https://github.com/MatheusKomatsu/SimpleMathPackage",
     packages=find_packages(),
     install_requires=requirements,
     python_requires='>=3.8',
