3d83c3e0e61394abc3ebbd0ec81e5abeccd7184c
Matheus Eiji Faria Komatsu <matheuskomatsu@hotmail.com>
2024-03-14 17:58:19 -0300

    SimpleMath
    This is a package with basic math operations, used exclusively for learning how to create and distribute packages.

--- README.md
+++ README.md
@@ -1,27 +1,27 @@
 # package_name
 
 Description. 
-The package package_name is used to:
-	- 
-	-
+The package simple_math is used to:
+	- Simple math
+This package is simply a test to understand how to create and distribute a package.
 
 ## Installation
 
-Use the package manager [pip](https://pip.pypa.io/en/stable/) to install package_name
+Use the package manager [pip](https://pip.pypa.io/en/stable/) to install simple_math
 
 ```bash
-pip install package_name
+pip install simple_math
 ```
 
 ## Usage
 
 ```python
-from package_name import file1_name
-file1_name.my_function()
+from simple_math import basic_operations
+basic_operations.add(number1, number2)
 ```
 
 ## Author
-My_name
+Matheus Eiji Faria Komatsu
 
 ## License
 [MIT](https://choosealicense.com/licenses/mit/)
--- setup.py
+++ setup.py
@@ -7,14 +7,14 @@ with open("requirements.txt") as f:
     requirements = f.read().splitlines()
 
 setup(
-    name="package_name",
+    name="simple_math",
     version="0.0.1",
-    author="my_name",
-    author_email="my_email",
-    description="My short description",
+    author="matheus_eiji_faria_komatsu",
+    author_email="matheuskomatsu@hotmail.com",
+    description="This package is used for simple math operations. It's used as an exercise to understand how to create and distribute a package.",
     long_description=page_description,
     long_description_content_type="text/markdown",
-    url="my_github_repository_project_link"
+    url="https://github.com/MatheusKomatsu/simple-package-template"
     packages=find_packages(),
     install_requires=requirements,
     python_requires='>=3.8',
